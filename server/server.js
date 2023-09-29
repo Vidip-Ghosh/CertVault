f2649a76ae6a0afb4e2a148c7f6a1c1ffced76d2
Vidip Ghosh <vidip2001@gmail.com>
2023-09-29 23:31:32 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello from the server');
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
