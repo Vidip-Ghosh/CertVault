ee89e3af6a959a7719064ad70001f255dcf713c1
Ogunnusi Abayomi Joseph <70065792+drsimplegraffiti@users.noreply.github.com>
2023-10-05 17:23:19 +0100

    Revamped the routes code to use async/await

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,86 +1,77 @@
-const router = require('express').Router();
-const User = require("../models/user.model")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const config = require("../config")
-const { verifyUserToken } = require("../middleware/auth")
+const router = require("express").Router();
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const { verifyUserToken } = require("../middleware/auth");
 
 
-router.route("/getUser").get((req,res)=>{
-    User.find()
-    .then(users =>{ 
-        console.log(users);
-        res.json(users)
-    })
-    .catch((e) => res.status(400).json('Error ' + e));
-
-})
-
-
-router.route("/signup").post(async(req,res)=>{
-    const salt = await bcrypt.genSalt(10);
-    console.log(req.body.password);
-    console.log(req.body.userName);
-    console.log(req.body.email)
-    const hasPassword = await bcrypt.hash(req.body.password, salt);
-    const userName = req.body.userName;
-    const email=  req.body.email ;
-    const password= hasPassword;
-    const newUser = new User({
-        userName:userName,
-        email : email ,
-        password : password
-    });
-
-    newUser.save().then((registeredUser)=>{
-        let payload = { id: registeredUser._id, user_type_id: req.body.user_type_id || 0 };
-            const token = jwt.sign(payload, config.JWT_SECRET);
-            res.status(200).send({ token })
-    }).catch((e)=>res.status(400).json('Error ' + e))
+router.route("/getUser").get(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (e) {
+    return res.status(400).json("Error " + e);
+  }
 });
 
 
-router.route("/login").post( async(req,res)=>{
-    console.log(req.body.userName)
-    User.findOne({userName : req.body.userName})
-    .then(async(user)=>{
-  
-            if (user) {
-                const validPass = await bcrypt.compare(req.body.password, user.password);
-               
-                if (!validPass) return res.status(401).send("Mobile/Email or Password is wrong");
+router.route("/signup").post(async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const { userName, email, password } = req.body;
+  const hasPassword = await bcrypt.hash(password, salt);
 
-                // Create and assign token
-                let payload = { id: user._id, user_type_id: user.user_type_id };
-                const token = jwt.sign(payload, config.JWT_SECRET);
+  // check if user already exists
+  const userExist = await User.findOne({ userName });
+  if (userExist) return res.status(409).json("User already exists");
+  const newUser = new User({
+    userName: userName,
+    email: email,
+    password: hasPassword,
+  });
 
-                res.status(200).header("auth-token", token).send({ "token": token });
-            }
-            else {
-                res.status(401).send('Invalid mobile')
-            }
+  try {
+    const registeredUser = await newUser.save();
+    let payload = {
+      id: registeredUser._id,
+      user_type_id: req.body.user_type_id || 0,
+    };
+    const token = jwt.sign(payload, config.JWT_SECRET);
+    res.status(200).json({ token });
+  } catch (e) {
+    return res.status(400).json("Error " + e);
+  }
+});
 
-        
-    })
-    .catch((e)=>{
-        console.log(e)
-        res.send(e);
-    })
 
-})
+router.route("/login").post(async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const userExist = await User.findOne({ userName });
+    if (!userExist) return res.status(401).json("Invalid username or password");
 
- 
+    const validPass = await bcrypt.compare(password, userExist.password);
+
+    if (!validPass) return res.status(401).json("Invalid username or password");
+
+    let payload = { id: userExist._id, user_type_id: userExist.user_type_id };
+    const token = jwt.sign(payload, config.JWT_SECRET);
+
+    res.status(200).json({ token });
+  } catch (e) {
+    return res.status(400).json("Error " + e);
+  }
+});
+
 router.route("/events").get(verifyUserToken);
 
-router.route("/getData").get((req,res)=>{
-    id = req.body.id;
-    User.findById(id)
-    .then(data =>{
-        console.log(data,"data");
-        res.json(data);
-    })
-    .catch((err)=>{
-        res.status(400).json("Error : " + err);
-    })
-})
-module.exports = router
+router.route("/getData").get(async (req, res) => {
+  try {
+    const id = req.body.id;
+    const data = await User.findById(id);
+    res.json(data);
+  } catch (e) {
+    return res.status(400).json("Error " + e);
+  }
+});
+module.exports = router;
